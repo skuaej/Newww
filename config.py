89b2eb839b5667e1687fbe15fe27e1a94ab0341d
skuaej <sumankumar821310@gmail.com>
2026-01-31 16:44:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
 API_ID = 27479878        # Your API ID (Integer, no quotes)
 API_HASH = "05f8dc8265d4c5df6376dded1d71c0ff"   # Your API Hash (String)
-BOT_TOKEN = "your_token" # Your Bot Token (String)
+BOT_TOKEN = "8082844410:AAFZODw1qsuezPFuci2XkahQJ4m_rUBNh3g" # Your Bot Token (String)
 AUTH_USERS = [7890781002, 6373737837] # Authorized User IDs
