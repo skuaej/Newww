654d8475f773d7bd1ebded92a0df61f950eac1f9
skuaej <sumankumar821310@gmail.com>
2026-01-31 15:06:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "20569947"
-api_hash = "d643430b289378849c13f2bdccb2de55"
-bot_token = "8356095578:AAF1izvPgTeCLBYpWgNAoDQHKlthSZcKPZI"
+api_id = "api"
+api_hash = "hash"
+bot_token = "bot token"
 auth_users = [7890781002,6373737837]
