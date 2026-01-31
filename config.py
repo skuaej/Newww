53e1f8f2705654323623fc3014de7494234fadd2
skuaej <sumankumar821310@gmail.com>
2026-01-31 15:02:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "enter your api_id"
-api_hash = "enter your api_hash"
-bot_token = "enter your bot token"
+api_id = "20569947"
+api_hash = "d643430b289378849c13f2bdccb2de55"
+bot_token = "8356095578:AAF1izvPgTeCLBYpWgNAoDQHKlthSZcKPZI"
 auth_users = [7890781002,6373737837]
